cf4f521fa9af455c48a60ed2b9b6b3b3d3325562
Zombieland4942 <zombieland4942@hotmail.com>
2020-04-17 08:59:23 +1000

    Added final chemical plant graphics

--- prototypes/func_chemical_plant.lua
+++ prototypes/func_chemical_plant.lua
@@ -2,8 +2,8 @@ function create_chemical_plant(args)
 
     local chemplant = table.deepcopy(data.raw["assembling-machine"]["chemical-plant-mk3"])
     chemplant.name = args.name
-    --chemplant.icon = "__built-in-beacons-fe-plus__/graphics/icons/" .. args.name .. ".png"
-    --chemplant.icon_size = 32
+    chemplant.icon = "__built-in-beacons-fe-plus__/graphics/icons/" .. args.name .. ".png"
+    chemplant.icon_size = 64
     chemplant.icon_mipmaps = nil
     chemplant.minable.result = args.name
     chemplant.max_health = 900
@@ -13,6 +13,11 @@ function create_chemical_plant(args)
     chemplant.energy_source = { type = "electric", usage_priority = "secondary-input", emissions_per_minute = args.emissions_per_minute, drain = args.energy_drain }    
     chemplant.allowed_effects = {"productivity","pollution"}
     
+    for _, direction in pairs({"north", "east", "south", "west"}) do
+        chemplant.animation[direction].layers[1].filename = "__built-in-beacons-fe-plus__/graphics/entity/beaconed-fe-chemical-plant/" .. chemplant.name .. "/chemical-plant.png"
+        chemplant.animation[direction].layers[1].hr_version.filename = "__built-in-beacons-fe-plus__/graphics/entity/beaconed-fe-chemical-plant/" .. chemplant.name .. "/hr-chemical-plant.png"
+    end
+
     data:extend({
         {
             type = "item",
