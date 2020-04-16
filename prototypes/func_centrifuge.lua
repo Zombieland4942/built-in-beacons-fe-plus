a9bd928bfb2fa23c09b501acb3db195b12800077
Zombieland4942 <zombieland4942@hotmail.com>
2020-04-17 08:34:59 +1000

    Added final centrifuge graphics

--- prototypes/func_centrifuge.lua
+++ prototypes/func_centrifuge.lua
@@ -2,8 +2,8 @@ function create_centrifuge(args)
 
     local centrifuge = table.deepcopy(data.raw["assembling-machine"]["centrifuge-mk3"])
     centrifuge.name = args.name
-    --centrifuge.icon = "__built-in-beacons-fe-plus__/graphics/icons/" .. args.name .. ".png"
-    --centrifuge.icon_size = 32
+    centrifuge.icon = "__built-in-beacons-fe-plus__/graphics/icons/" .. args.name .. ".png"
+    centrifuge.icon_size = 64
     centrifuge.icon_mipmaps = nil
     centrifuge.minable.result = args.name
     centrifuge.max_health = 900
@@ -13,6 +13,38 @@ function create_centrifuge(args)
     centrifuge.energy_source = { type = "electric", usage_priority = "secondary-input", emissions_per_minute = args.emissions_per_minute, drain = args.energy_drain }    
     centrifuge.allowed_effects = {"productivity","pollution"}
     
+    if centrifuge.idle_animation then
+        centrifuge.idle_animation.layers[1].filename = "__built-in-beacons-fe-plus__/graphics/entity/beaconed-fe-centrifuge/" .. args.name .. "/centrifuge-C.png"
+        centrifuge.idle_animation.layers[1].hr_version.filename = "__built-in-beacons-fe-plus__/graphics/entity/beaconed-fe-centrifuge/" .. args.name .. "/hr-centrifuge-C.png"
+        centrifuge.idle_animation.layers[3].filename = "__built-in-beacons-fe-plus__/graphics/entity/beaconed-fe-centrifuge/" .. args.name .. "/centrifuge-B.png"
+        centrifuge.idle_animation.layers[3].hr_version.filename = "__built-in-beacons-fe-plus__/graphics/entity/beaconed-fe-centrifuge/" .. args.name .. "/hr-centrifuge-B.png"
+        centrifuge.idle_animation.layers[5].filename = "__built-in-beacons-fe-plus__/graphics/entity/beaconed-fe-centrifuge/" .. args.name .. "/centrifuge-A.png"
+        centrifuge.idle_animation.layers[5].hr_version.filename = "__built-in-beacons-fe-plus__/graphics/entity/beaconed-fe-centrifuge/" .. args.name .. "/hr-centrifuge-A.png"
+
+        centrifuge.idle_animation.layers[1].animation_speed = 0.1
+        centrifuge.idle_animation.layers[1].hr_version.animation_speed = 0.1
+        centrifuge.idle_animation.layers[3].animation_speed = 0.1
+        centrifuge.idle_animation.layers[3].hr_version.animation_speed = 0.1
+        centrifuge.idle_animation.layers[5].animation_speed = 0.1
+        centrifuge.idle_animation.layers[5].hr_version.animation_speed = 0.1
+    end
+
+    if centrifuge.animation then
+        centrifuge.animation.layers[1].filename = "__built-in-beacons-fe-plus__/graphics/entity/beaconed-fe-centrifuge/" .. args.name .. "/centrifuge-C-light.png"
+        centrifuge.animation.layers[1].hr_version.filename = "__built-in-beacons-fe-plus__/graphics/entity/beaconed-fe-centrifuge/" .. args.name .. "/hr-centrifuge-C-light.png"
+        centrifuge.animation.layers[2].filename = "__built-in-beacons-fe-plus__/graphics/entity/beaconed-fe-centrifuge/" .. args.name .. "/centrifuge-B-light.png"
+        centrifuge.animation.layers[2].hr_version.filename = "__built-in-beacons-fe-plus__/graphics/entity/beaconed-fe-centrifuge/" .. args.name .. "/hr-centrifuge-B-light.png"
+        centrifuge.animation.layers[3].filename = "__built-in-beacons-fe-plus__/graphics/entity/beaconed-fe-centrifuge/" .. args.name .. "/centrifuge-A-light.png"
+        centrifuge.animation.layers[3].hr_version.filename = "__built-in-beacons-fe-plus__/graphics/entity/beaconed-fe-centrifuge/" .. args.name .. "/hr-centrifuge-A-light.png"
+        
+        centrifuge.animation.layers[1].animation_speed = 0.1
+        centrifuge.animation.layers[1].hr_version.animation_speed = 0.1
+        centrifuge.animation.layers[2].animation_speed = 0.1
+        centrifuge.animation.layers[2].hr_version.animation_speed = 0.1
+        centrifuge.animation.layers[3].animation_speed = 0.1
+        centrifuge.animation.layers[3].hr_version.animation_speed = 0.1
+    end
+
     data:extend({
         {
             type = "item",
