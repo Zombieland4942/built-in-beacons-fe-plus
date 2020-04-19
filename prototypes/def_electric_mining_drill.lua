d1f1ecb506b8a924df1fcf84eec72d7c0d6d6e2e
Zombieland4942 <zombieland4942@hotmail.com>
2020-04-19 18:28:27 +1000

    Updated mining drills

--- prototypes/def_electric_mining_drill.lua
+++ prototypes/def_electric_mining_drill.lua
@@ -1,9 +1,9 @@
 --
--- electric-furnace                                 md1         md2         md3
+-- electric-drill                                   ed1         ed2         ed3
 -- module type                                      s4          s5          s6
 -- mining_speed                                     48.4        71.6        100.6
 -- energy_source.emissions_per_minute               145.2       162         180
--- energy_usage                                     14MW        15MW        16MW
+-- energy_usage                                     9.46MW      10.78MW     12.1MW
 --
 
 require ("func_electric_mining_drill")
@@ -11,9 +11,8 @@ require ("func_electric_mining_drill")
 local beaconed_fe_electric_mining_drill_1 = { 
                                                 name = "beaconed-fe-electric-mining-drill-1",  
                                                 mining_speed = 48.4,
-                                                energy_usage = "14MW",
-                                                energy_drain = "7MW",
-                                                emissions_per_minute = 145.2,
+                                                energy_usage = "9.46MW",
+                                                emissions_per_minute = 151.2,
                                                 ingredients =
                                                 {
                                                     {"electric-mining-drill-mk3", 1},
@@ -28,9 +27,8 @@ create_electric_mining_drill(beaconed_fe_electric_mining_drill_1)
 local beaconed_fe_electric_mining_drill_2 = { 
                                                 name = "beaconed-fe-electric-mining-drill-2",  
                                                 mining_speed = 71.6,
-                                                energy_usage = "15MW",
-                                                energy_drain = "7MW",
-                                                emissions_per_minute = 162,
+                                                energy_usage = "10.78MW",
+                                                emissions_per_minute = 171.6,
                                                 ingredients =
                                                 {
                                                     {"electric-mining-drill-mk3", 1},
@@ -45,9 +43,8 @@ create_electric_mining_drill(beaconed_fe_electric_mining_drill_2)
 local beaconed_fe_electric_mining_drill_3 = { 
                                                 name = "beaconed-fe-electric-mining-drill-3",  
                                                 mining_speed = 100.6,
-                                                energy_usage = "16MW",
-                                                energy_drain = "7MW",
-                                                emissions_per_minute = 180,
+                                                energy_usage = "12.1MW",
+                                                emissions_per_minute = 192,
                                                 ingredients =
                                                 {
                                                     {"electric-mining-drill-mk3", 1},
