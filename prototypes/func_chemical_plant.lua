9c85405f16f279570879260136e2db96194df103
Zombieland4942 <zombieland4942@hotmail.com>
2020-04-19 18:11:56 +1000

    - Updated specs for Chemical Plant

--- prototypes/func_chemical_plant.lua
+++ prototypes/func_chemical_plant.lua
@@ -5,12 +5,11 @@ function create_chemical_plant(args)
     chemplant.icon = "__built-in-beacons-fe-plus__/graphics/icons/" .. args.name .. ".png"
     chemplant.icon_size = 64
     chemplant.icon_mipmaps = nil
-    chemplant.minable.result = args.name
-    chemplant.max_health = 900
+    chemplant.minable.result = args.name    
     chemplant.module_specification.module_slots = 8
     chemplant.crafting_speed = args.crafting_speed
     chemplant.energy_usage = args.energy_usage
-    chemplant.energy_source = { type = "electric", usage_priority = "secondary-input", emissions_per_minute = args.emissions_per_minute, drain = args.energy_drain }    
+    chemplant.energy_source = { type = "electric", usage_priority = "secondary-input", emissions_per_minute = args.emissions_per_minute, drain = "6.7MW" }    
     chemplant.allowed_effects = {"productivity","pollution"}
     
     for _, direction in pairs({"north", "east", "south", "west"}) do
