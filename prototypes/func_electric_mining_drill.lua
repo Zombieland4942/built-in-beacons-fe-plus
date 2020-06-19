36e636e902f0c2d708ace0d5fc36be7542d0dd6f
Zombieland4942 <zombieland4942@hotmail.com>
2020-06-19 18:38:35 +1000

    Removed custom graphics, now works in 0.18.32

--- prototypes/func_electric_mining_drill.lua
+++ prototypes/func_electric_mining_drill.lua
@@ -13,17 +13,17 @@ function create_electric_mining_drill(args)
     mining_drill.energy_source = { type = "electric", usage_priority = "secondary-input", emissions_per_minute = args.emissions_per_minute, drain = "6.7MW" }    
     mining_drill.allowed_effects = {}
     
-    for _, direction in pairs({"north", "east", "south", "west"}) do
-        local short = string.upper(string.sub(direction, 1, 1))
-        -- animations        
-        mining_drill.animations[direction].filename = "__built-in-beacons-fe-plus__/graphics/entity/beaconed-fe-electric-mining-drill/" .. mining_drill.name .. "/electric-mining-drill-" .. short .. ".png"
-        mining_drill.animations[direction].animation_speed = 2
-        mining_drill.animations[direction].hr_version.filename = "__built-in-beacons-fe-plus__/graphics/entity/beaconed-fe-electric-mining-drill/" .. mining_drill.name .. "/hr-electric-mining-drill-" .. short .. ".png"
-        mining_drill.animations[direction].hr_version.animation_speed = 2
-        -- input_fluid_patch_sprites
-        mining_drill.input_fluid_patch_sprites[direction].filename = "__built-in-beacons-fe-plus__/graphics/entity/beaconed-fe-electric-mining-drill/" .. mining_drill.name .. "/electric-mining-drill-" .. short .. "-patch.png"
-        mining_drill.input_fluid_patch_sprites[direction].hr_version.filename = "__built-in-beacons-fe-plus__/graphics/entity/beaconed-fe-electric-mining-drill/" .. mining_drill.name .. "/hr-electric-mining-drill-" .. short .. "-patch.png"
-    end
+    --for _, direction in pairs({"north", "east", "south", "west"}) do
+    --    local short = string.upper(string.sub(direction, 1, 1))
+    --    -- animations        
+    --    mining_drill.animations[direction].filename = "__built-in-beacons-fe-plus__/graphics/entity/beaconed-fe-electric-mining-drill/" .. mining_drill.name .. "/electric-mining-drill-" .. short .. ".png"
+    --    mining_drill.animations[direction].animation_speed = 2
+    --    mining_drill.animations[direction].hr_version.filename = "__built-in-beacons-fe-plus__/graphics/entity/beaconed-fe-electric-mining-drill/" .. mining_drill.name .. "/hr-electric-mining-drill-" .. short .. ".png"
+    --    mining_drill.animations[direction].hr_version.animation_speed = 2
+    --    -- input_fluid_patch_sprites
+    --    mining_drill.input_fluid_patch_sprites[direction].filename = "__built-in-beacons-fe-plus__/graphics/entity/beaconed-fe-electric-mining-drill/" .. mining_drill.name .. "/electric-mining-drill-" .. short .. "-patch.png"
+    --    mining_drill.input_fluid_patch_sprites[direction].hr_version.filename = "__built-in-beacons-fe-plus__/graphics/entity/beaconed-fe-electric-mining-drill/" .. mining_drill.name .. "/hr-electric-mining-drill-" .. short .. "-patch.png"
+    --end
 
     local result,ingredients
 
